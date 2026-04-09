395626c10662f7d41a5e75a93c9bbc161043c2a9
Atticli <Atticlmr@gmail.com>
2026-04-09 15:29:30 +0800

    Update default version to v0.1.0

--- install.sh
+++ install.sh
@@ -6,7 +6,7 @@ set -e
 
 REPO="${REPO:-Atticlmr/atop-monitor}"
 INSTALL_PREFIX="${INSTALL_PREFIX:-/usr/local}"
-VERSION="${VERSION:-v0.0.3}"
+VERSION="${VERSION:-v0.1.0}"
 TMPDIR=$(mktemp -d)
 ARCH=$(uname -m)
 OS=$(uname -s)
